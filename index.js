84cf5cd267bfe5f92d475e747c5190b6f74c91e7
Samantha-Ruth <sam.haberman@gmail.com>
2023-09-28 21:18:17 -0600

    changed circle to hexagon

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { Engine, Render, Runner, World, Bodies , MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } =
+  Matter;
 
 const width = 800;
 const height = 600;
@@ -6,46 +7,53 @@ const height = 600;
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
-    element: document.body,
-    engine: engine,
-    options: {
-        wireframes: false,
-        width, 
-        height
-    }
+  element: document.body,
+  engine: engine,
+  options: {
+    wireframes: false,
+    width,
+    height,
+  },
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}));
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
 
 // Walls
 const walls = [
-    Bodies.rectangle(400,0,800,40, {isStatic: true}),
-    Bodies.rectangle(400,600,800,40, {isStatic: true}),
-    Bodies.rectangle(0,300,40,600, {isStatic: true}),
-    Bodies.rectangle(800,300,40,600, {isStatic: true})
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
 ];
 World.add(world, walls);
 
 // random shapes
 
 for (let i = 0; i < 20; i++) {
-    if (Math.random() > 0.5){
-World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50,{
-    render: {
-      fillStyle: 'blue'
-    }
-}
- ))
-} else {
-    World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35, {
+  const random = Math.random();
+  if (random > 0.5) {
+    World.add(
+      world,
+      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50, {
         render: {
-          fillStyle: 'purple'
-        }
-    })
-    )
-}
+          fillStyle: "blue",
+        },
+      })
+    );
+  } else {
+    World.add(
+      world, Bodies.polygon(Math.random() * width, Math.random() * height, 5, 50,{
+        render: {
+          fillStyle: "purple",
+        },
+      })
+    );
+  }
 }
